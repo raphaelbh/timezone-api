8bf5b4317b691e50aafa3670e98a8b72ee79cd44
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-02-07 16:00:47 -0300

    fix flake8 issue

--- tests/api/views_test.py
+++ tests/api/views_test.py
@@ -6,12 +6,12 @@ class ApiViewsTest(unittest.TestCase):
     def test_timezones_request(self):
         client = app.test_client(self)
         response = client.get('/api/v1/timezoness')
-        assert response.status_code is 200, "Should return status code 200"
+        assert response.status_code == 200, "Should return status code 200"
 
     def test_now_request(self):
         client = app.test_client(self)
         response = client.get('/api/v1/now')
-        assert response.status_code is 200, "Should return status code 200"
+        assert response.status_code == 200, "Should return status code 200"
 
 if __name__ == '__main__':
     unittest.main()
