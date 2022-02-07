e21d3a72e70e7c03961d63841a5ede398df2bbac
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-02-07 16:04:02 -0300

    fix unit test

--- tests/api/views_test.py
+++ tests/api/views_test.py
@@ -5,7 +5,7 @@ class ApiViewsTest(unittest.TestCase):
 
     def test_timezones_request(self):
         client = app.test_client(self)
-        response = client.get('/api/v1/timezoness')
+        response = client.get('/api/v1/timezones')
         assert response.status_code == 200, "Should return status code 200"
 
     def test_now_request(self):
