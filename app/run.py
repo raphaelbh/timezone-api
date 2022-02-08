ea3f149368c4f24dbc68c05aa06ad8579363b3e4
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-02-08 16:59:02 -0300

    add swagger ui on api

--- app/run.py
+++ app/run.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api import api_blueprint
+from swagger import swagger_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+app.register_blueprint(swagger_blueprint)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
