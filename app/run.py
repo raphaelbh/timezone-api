7ab84e9f386ead1b01d7e2bc46337bdaee396969
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-02-08 17:17:33 -0300

    fix remote swagger cors error

--- app/run.py
+++ app/run.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from api import api_blueprint
 from swagger import swagger_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 app.register_blueprint(swagger_blueprint)
+
+cors = CORS(app)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
