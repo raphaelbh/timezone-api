4db4ead4870a76f3e233cca436653e8d9d083785
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-02-09 10:40:42 -0300

    add convertion by informed timezone

--- app/timezone/timezone.py
+++ app/timezone/timezone.py
@@ -1,8 +1,26 @@
 import pytz
-from datetime import datetime;
 
 def timezones(): 
     return pytz.all_timezones
 
-def now():
-    return datetime.now()
+def convert_datetime(datetime, timezoneParam):
+
+    if (timezoneParam != None and timezoneParam not in pytz.all_timezones):
+        raise Exception('Invalid timezone')
+
+    result = {}
+    utc_datetime = __convert_datetime(datetime, 'UTC')
+    result['UTC'] = _format_datetime(utc_datetime)
+
+    if (timezoneParam != None):
+        utc_datetime = __convert_datetime(datetime, timezoneParam)
+        result[timezoneParam] = _format_datetime(utc_datetime)
+
+    return result
+
+def __convert_datetime(datetime, zone):
+    timezone = pytz.timezone(zone)
+    return datetime.astimezone(timezone)
+
+def _format_datetime(datetime): 
+    return datetime.strftime('%Y-%m-%d %H:%M:%S %Z%z')
