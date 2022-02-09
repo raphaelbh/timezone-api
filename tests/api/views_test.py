02423e17d0f0d9de6d9e998a182dff74bf52b890
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-02-09 10:41:00 -0300

    unit tests

--- tests/api/views_test.py
+++ tests/api/views_test.py
@@ -12,6 +12,11 @@ class ApiViewsTest(unittest.TestCase):
         client = app.test_client(self)
         response = client.get('/api/v1/now')
         assert response.status_code == 200, "Should return status code 200"
+    
+    def test_now_request_with_invalid_timezone(self):
+        client = app.test_client(self)
+        response = client.get('/api/v1/now?timezone=America/Sao_P')
+        assert response.status_code == 400, "Should return status code 400"
 
 if __name__ == '__main__':
     unittest.main()
--- tests/timezone/timezone_test.py
+++ tests/timezone/timezone_test.py
@@ -1,4 +1,5 @@
 import unittest
+import datetime
 from app.timezone import timezone
 
 class TimezoneTest(unittest.TestCase):
@@ -6,8 +7,22 @@ class TimezoneTest(unittest.TestCase):
     def test_timezones_is_not_none(self):
         assert len(timezone.timezones()) > 0, "Should be a list with values"
 
-    def test_now_is_not_none(self):
-        assert timezone.now() != None, "Should have the current date"
+    def test_convert_datetime_with_no_timezone_informed(self):
+        current_datetime = datetime.datetime(2011, 11, 4, 13, 5, 23, 283000, tzinfo=datetime.timezone.utc)
+        actual = timezone.convert_datetime(current_datetime, None)
+        expected = { 'UTC': '2021-11-04 13:05:23 UTC+0000' }
+        assert all([a == b for a, b in zip(actual, expected)])
+
+    def test_convert_datetime_correct_timezone_informed(self):
+        current_datetime = datetime.datetime(2011, 11, 4, 13, 5, 23, 283000, tzinfo=datetime.timezone.utc)
+        actual = timezone.convert_datetime(current_datetime, 'America/Sao_Paulo') 
+        expected = { 'UTC':'2021-11-04 13:05:23 UTC+0000', 'America/Sao_Paulo':'2021-11-04 10:05:23 -03-0300' }
+        assert all([a == b for a, b in zip(actual, expected)])
+
+    def test_convert_datetime_invalid_timezone_informed(self):
+        current_datetime = datetime.datetime(2011, 11, 4, 13, 5, 23, 283000, tzinfo=datetime.timezone.utc)
+        with self.assertRaises(Exception):
+            timezone.convert_datetime(current_datetime, 'America/Sao_P')
 
 if __name__ == '__main__':
     unittest.main()
